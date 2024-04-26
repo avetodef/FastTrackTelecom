853dbd5177dfc60f75e6b5068ee7a038ed722a44
Sofia <93034945+avetodef@users.noreply.github.com>
2024-04-26 23:17:59 +0300

    Delete FastTrackTask3 directory

--- FastTrackTask3/build.sh
+++
@@ -1,8 +0,0 @@
-# rm -r src/*
-# rm -r build/*
-
-
-asn1c RRC.asn1 -D src -no-gen-OER -no-gen-example
-
-gcc -Isrc -o build/client client.c CodeDecode.c src/*.c -DASN_DISABLE_OER_SUPPORT -lsctp
-gcc -Isrc -o build/server server.c CodeDecode.c src/*.c -DASN_DISABLE_OER_SUPPORT -lsctp
